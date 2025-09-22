83a656f33b10918eb21a891f762bd28b4336577f
Antek <antonikamykowskibiznes@gmail.com>
2025-09-22 18:30:57 +0200

    Solved3

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -7,21 +7,22 @@ function calculateRentalCost(days) {
   const dailyRate = 40;
   const discountFor7Days = 50;
   const discountFor3Days = 20;
+  const longTermRentThreshold = 7;
+  const shortTermRentThreshold = 3;
   let result = days * dailyRate;
 
-  if (days >= 7) {
+  if (days >= longTermRentThreshold) {
     result = result - discountFor7Days;
 
     return result;
   }
 
-  if (days >= 3) {
+  if (days >= shortTermRentThreshold) {
     result = result - discountFor3Days;
 
     return result;
-  } else {
-    return result;
   }
+  return result;
 }
 
 module.exports = calculateRentalCost;
